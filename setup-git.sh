32409000379251d750e2c39a44aaf6e5e88a05f8
Guo Shiwei <guoshiwei@sogou-inc.com>
2013-04-08 12:43:08 +0800

    Add gitconfig and use it in setup.sh

--- setup-git.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git config --global user.name 'Guo Shiwei'
-git config --global user.email 'guoshiwei@gmail.com'
-git config --global color.ui true
-git config --global i18n.commitencoding gbk
-git config --global i18n.logoutputencoding gbk
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-git config --global user.name guoshiwei
-git config --global user.email guoshiwei@gmail.com
-git config --global color.ui true
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
-
+ln -s ~/mylinuxconf/gitconfig ~/.gitconfig -f
 # Install
 # Linux man pages
 # yum install man-pages
